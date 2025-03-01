98ab81ef7612be8f4d2d83a50bb239a0228c535c
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2025-03-01 14:14:55 -0800

    Update messages.js

--- pages/messages.js
+++ pages/messages.js
@@ -2,70 +2,70 @@ const messages = {
     // Clicks on the 'No Login' button if present
     signInWithoutLogin: function () {
         return this
-            .waitForElementPresent('@noLogin')
+            .waitForElementPresent('@noLogin', 'sign in with Login button is present')
             .click('@noLogin');
     },
 
     // Starts a new chat by clicking on the 'Start Chat' button
     selectionStartChat: function () {
-        return this.waitForElementPresent('@startChat')
+        return this.waitForElementPresent('@startChat', 'Start Chat button is present')
             .click('@startChat');
     },
 
     // Selects the 'Create Group' option
     selectCreateGroup: function () {
-        return this.waitForElementPresent('@createGroup')
+        return this.waitForElementPresent('@createGroup', 'Create Group button is present')
             .click('@createGroup');
     },
 
     // Enters a phone number into the input field
     enterPhoneNumber: function (phoneNumber) {
-        return this.waitForElementPresent('@inputNumber')
+        return this.waitForElementPresent('@inputNumber', 'Phone number input field is present')
             .setValue('@inputNumber', phoneNumber)
             .selectTickMark();
     },
 
     // Selects the tick mark button (usually to confirm a selection)
     selectTickMark: function () {
-        return this.waitForElementPresent('@tickMark')
+        return this.waitForElementPresent('@tickMark', 'Tick mark button is present')
             .click('@tickMark');
     },
 
     // Enters a group name into the input field
     enterGroupName: function (groupName) {
-        return this.waitForElementPresent('@inputGroupName')
+        return this.waitForElementPresent('@inputGroupName', 'Group name input field is present')
             .setValue('@inputGroupName', groupName)
             .assert.textContains('@inputGroupName', groupName);
     },
 
     // Clicks on the 'Next' button to proceed
     selectNextButton: function () {
-        return this.waitForElementPresent('@nextButton')
+        return this.waitForElementPresent('@nextButton', 'Next button is present')
             .click('@nextButton');
     },
 
     // Clicks on the 'Done' button to finalize an action
     selectDoneButton: function () {
-        return this.waitForElementPresent('@doneButton')
+        return this.waitForElementPresent('@doneButton', 'Done button is present')
             .click('@doneButton');
     },
 
     // Enters a message into the message input field
     enterMessage: function (message) {
-        return this.waitForElementPresent('@messageInput')
+        return this.waitForElementPresent('@messageInput', 'Message input field is present')
             .setValue('@messageInput', message)
             .assert.textContains('@messageInput', message);
     },
 
     // Clicks on the 'Send' button to send a message
     selectSend: function () {
-        return this.waitForElementPresent('@sendButton')
+        return this.waitForElementPresent('@sendButton', 'Send button is present')
             .click('@sendButton');
     },
 
-    //Verify sent message
+    //Verifies that a message has been sent
     verifyMessageSent: function (message) {
-        return this.waitForElementPresent('@sendMessage')
+        return this.waitForElementPresent('@sendMessage', 'Message sent text is present')
             .assert.textContains('@sendMessage', message);
     }
 };
